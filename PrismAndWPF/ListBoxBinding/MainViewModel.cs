c81d9737fea8bc7727a73ab38ef3cfe81bed722e
wby <wubuyuen@sina.com>
2021-02-02 23:08:26 +0800

    add ListBoxBinding

---
+++ PrismAndWPF/ListBoxBinding/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ListBoxBinding
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<MenuModel> MenuModels { get; set; }
+
+        public MainViewModel()
+        {
+            MenuModels = new ObservableCollection<MenuModel>();
+            MenuModels.Add(new MenuModel() { IconFont = "\xe608", Title = "Live scores", });
+            MenuModels.Add(new MenuModel() { IconFont = "\xe620", Title = "Series", });
+            MenuModels.Add(new MenuModel() { IconFont = "\xe622", Title = "Teams", });
+            MenuModels.Add(new MenuModel() { IconFont = "\xe603", Title = "Features", });
+            MenuModels.Add(new MenuModel() { IconFont = "\xe51c", Title = "Videos", });
+            MenuModels.Add(new MenuModel() { IconFont = "\xe790", Title = "Stats", });
+            MenuModels.Add(new MenuModel() { IconFont = "\xe672", Title = "World cup 2019", });
+        }
+    }
+}
---
+++ PrismAndWPF/ListBoxBinding/MenuModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ListBoxBinding
+{
+    public class MenuModel
+    {
+        public string IconFont { get; set; }
+        public string Title { get; set; }
+    }
+}
