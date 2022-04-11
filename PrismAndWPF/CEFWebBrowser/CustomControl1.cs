7e772da1b87d1ef3562e68c6588d81fe323849b6
Wby <Wby@WBY-PC>
2022-04-11 19:34:47 +0800

    添加WebBrowser模块

---
+++ PrismAndWPF/CEFWebBrowser/CustomControl1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CEFWebBrowser
+{
+    public class CustomControl1 : Button
+    {
+        static CustomControl1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
+        }
+    }
+}
